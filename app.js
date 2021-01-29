ac542433a6700dfc1b210f1ebd5d034623f47adb
Isabelle Parker <iparker@ucsd.edu>
2021-01-29 11:58:03 +1000

    lab4 finished

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var hello = require('./routes/hello');
+var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
 
 var app = express();
 
 // all environments
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars());
@@ -29,6 +31,7 @@ app.use(express.cookieParser('IxD secret key'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.get("/project",project.viewProject);
 
 // development only
 if ('development' == app.get('env')) {
@@ -39,6 +42,7 @@ if ('development' == app.get('env')) {
 app.get('/', index.view);
 app.get('/hello/:userName', hello.view);
 // Example route
+app.get("/project/:name", project.viewProject);
 // app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
